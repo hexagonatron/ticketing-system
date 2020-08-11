d0625cf43e94589228d5bb5cf6c6a77835253912
Ben <46476247+hexagonatron@users.noreply.github.com>
2020-08-11 16:50:56 +0930

    server

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 //Dependencies
-var express = require('express');
-// require ('dotenv').config();
+const express = require('express');
+require ('dotenv').config();
 
 const path = require('path');
 
 // Sets up the Express App
-var app = express();
+const app = express();
 
-var PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 console.log(`Port is ${PORT}`)
 
 // Requiring our models for syncing
-var db = require("./models");
+const db = require("./models");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
