42186a3d2810519bf655f74397754fa0e5a6b610
Ben <>
2021-08-09 03:22:26 +0930

    Creating proper static file directory.

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use("/api/*", (req, res) => res.status(404).json({error: "Invalid API route"
 
 //Handle Prod
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client", "build")));
 }
 
 app.get("*", (req, res) => {
